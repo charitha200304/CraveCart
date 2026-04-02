b4974c20363a8e9bf89c20b8055ff5f8f39f476b
ASUS <charithachiranjeewa@gmail.com>
2026-04-02 23:54:16 +0530

    initialize CraveCart project with Spring Boot and Maven setup

---
+++ src/main/java/com/cravecart/backend/CraveCartApplication.java
@@ -0,0 +1,13 @@
+package com.cravecart.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraveCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CraveCartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cravecart/backend/CraveCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cravecart.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraveCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
