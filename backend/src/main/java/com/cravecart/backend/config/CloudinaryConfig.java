98346eb02233dfdabdf4c5c685d7ea7641db7a60
ASUS <charithachiranjeewa@gmail.com>
2026-05-14 11:56:45 +0530

    feat: add Dockerfile for production deployment and Cloudinary configuration bean

--- backend/src/main/java/com/cravecart/backend/config/CloudinaryConfig.java
+++ backend/src/main/java/com/cravecart/backend/config/CloudinaryConfig.java
@@ -13,9 +13,9 @@ public class CloudinaryConfig {
     @Bean
     public Cloudinary cloudinary() {
         Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", "djzobuyts");
-        config.put("api_key", "155468976843393");
-        config.put("api_secret", "di8R8BlmWOT1azFeaqO6IUbvvoM");
+        config.put("cloud_name", System.getenv().getOrDefault("CLOUDINARY_NAME", "djzobuyts"));
+        config.put("api_key", System.getenv().getOrDefault("CLOUDINARY_KEY", "155468976843393"));
+        config.put("api_secret", System.getenv().getOrDefault("CLOUDINARY_SECRET", "di8R8BlmWOT1azFeaqO6IUbvvoM"));
         return new Cloudinary(config);
     }
 }
