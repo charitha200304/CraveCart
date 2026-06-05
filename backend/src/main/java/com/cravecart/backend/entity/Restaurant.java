76985b1659ad5346f8833460ed9da1bb59254901
ASUS <charithachiranjeewa@gmail.com>
2026-06-06 00:16:08 +0530

    feat: add Restaurant entity with JPA mapping and associations

--- backend/src/main/java/com/cravecart/backend/entity/Restaurant.java
+++ backend/src/main/java/com/cravecart/backend/entity/Restaurant.java
@@ -3,6 +3,7 @@ package com.cravecart.backend.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Entity
